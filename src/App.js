b54523de18e5d2be99b9108128158c6ea4b2beff
Alex Engelmann <alex.engelmann@gmail.com>
2019-10-02 16:07:14 -0700

    added form and logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import './App.css';
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {/*<Logo></Logo>
-      <ImageLinkForm></ImageLinkForm>
-      <FaceRecognition></FaceRecognition>*/}
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/*<FaceRecognition />*/}
     </div>
   );
 }
