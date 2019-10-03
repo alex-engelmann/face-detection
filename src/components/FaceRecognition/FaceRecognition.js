7d7a8673cabd4cbb62605d1d905ee0a5a411e079
Alex Engelmann <alex.engelmann@gmail.com>
2019-10-03 01:03:20 -0700

    facial recognition working, box drawn

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl }) => {
+const FaceRecognition = ({ imageUrl, box }) => {
     return (
         <div className='center ma' >
             <div className='absolute mt2'>
-                <img width='500px' height='auto' alt='sample' src={imageUrl} />
+                <img id='inputImage' width='500px' height='auto' alt='sample' src={imageUrl} />
+                <div className='bounding-box'
+                style={{
+                    top: box.topRow,
+                    right: box.rightCol,
+                    bottom: box.bottomRow,
+                    left: box.leftCol
+                }}
+                ></div>
+            
             </div>
         </div>
     );
