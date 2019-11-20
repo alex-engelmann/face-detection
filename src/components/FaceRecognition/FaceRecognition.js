bad1449e2563dd0035fb72d2c8429be45cfcb5d3
Alex Engelmann <alex.engelmann@gmail.com>
2019-11-20 02:31:52 -0800

    removed all console warnings

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -6,32 +6,22 @@ const FaceRecognition = ({ imageUrl, box }) => {
         <div className='center ma' >
             <div className='absolute mt2'>
                 <img id='inputImage' width='500px' height='auto' alt='user submission' src={imageUrl} />
-                {console.log(box)}
+                {/*console.log(box)}
                 {console.log(box.finalfaces["0"].bottomRow)}
 
                 {/*This goes through all the array elements and creates boxes */}
 
-                { box.finalfaces.map((ele) => (
-                    <div className='bounding-box'
-                    style={{
-                        top: ele.topRow,
-                        right: ele.rightCol,
-                        bottom: ele.bottomRow,
-                        left: ele.leftCol
-                    }}
-                ></div>
+                {box.finalfaces.map((ele, index) => (
+
+                    <div key={index} className='bounding-box'
+                        style={{
+                            top: ele.topRow,
+                            right: ele.rightCol,
+                            bottom: ele.bottomRow,
+                            left: ele.leftCol
+                        }}
+                    ></div>
                 ))}
-               
-                {/*This was the old way*/}
-                    
-                {/*<div className='bounding-box'
-                    style={{
-                        top: box.finalfaces[0].topRow,
-                        right: box.finalfaces[0].rightCol,
-                        bottom: box.finalfaces[0].bottomRow,
-                        left: box.finalfaces[0].leftCol
-                    }}
-                ></div>*/}
 
             </div>
         </div>
