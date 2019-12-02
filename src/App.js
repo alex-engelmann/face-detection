b94a1740e0be2db79c9c459e079c72f79a9c6b42
Alex Engelmann <alex.engelmann@gmail.com>
2019-12-02 15:50:28 -0500

    removed clear on focus, clear on submit is enough

--- src/App.js
+++ src/App.js
@@ -153,7 +153,6 @@ class App extends Component {
                 entries={this.state.user.entries}
               /> : ""}
             <ImageLinkForm
-              clearURL={this.clearURL}
               onInputChange={this.onInputChange}
               onPictureSubmit={this.onPictureSubmit}
               oneFace={this.oneFace}
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = ({ clearURL, onInputChange, onPictureSubmit, oneFace, manyFaces }) => {
+const ImageLinkForm = ({ onInputChange, onPictureSubmit, oneFace, manyFaces }) => {
     return (
         <div>
             <p className='f3'>
                 {'This site will find the faces in your pictures.  Enter the URL of an image or try these examples and click detect to see it in action!'}
             </p>
             <p>
-                <button className='f5 link dim br3 ph3 pv2 mb2 dib white bg-dark-green' type="submit" onClick={oneFace}>One Face</button>
-                <button className='f5 link dim br3 ph3 pv2 mb2 dib white bg-dark-green' type="submit" onClick={manyFaces}>Many Faces</button>
+                <button className='f5 link dim br3 ph3 pv2 mb2 dib white bg-dark-green'
+                 type="submit" onClick={oneFace}>One Face</button>
+                <button className='f5 link dim br3 ph3 pv2 mb2 dib white bg-dark-green'
+                 type="submit" onClick={manyFaces}>Many Faces</button>
             </p>
 
 
             <div className='center'>
                 <div className='form center pa4 br3 shadow-5'>
 
-                    <input id='urlfield' className='f3 pa2 w-70 center' type='text' onFocus={clearURL} onChange={onInputChange} />
+                    <input id='urlfield' className='f3 pa2 w-70 center' type='text' onChange={onInputChange} />
                     <button
                         type="submit"
                         className='w-30 f4 link ph1 pv2 dib white bg-orange'
