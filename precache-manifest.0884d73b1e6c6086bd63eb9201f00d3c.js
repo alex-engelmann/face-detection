de7829bdb09524f83b3e06592a618d39d0eb5b1c
Alex Engelmann <alex.engelmann@gmail.com>
2019-12-02 03:57:46 -0500

    Updates

---
+++ precache-manifest.0884d73b1e6c6086bd63eb9201f00d3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c813c7eb3cee0480af3e58cb2161d442",
+    "url": "/face-detection/index.html"
+  },
+  {
+    "revision": "2e4f1920da5359e98714",
+    "url": "/face-detection/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "4d80c98cf3cf26826d04",
+    "url": "/face-detection/static/css/main.2bbc4ab3.chunk.css"
+  },
+  {
+    "revision": "2e4f1920da5359e98714",
+    "url": "/face-detection/static/js/2.abf7bfec.chunk.js"
+  },
+  {
+    "revision": "4d80c98cf3cf26826d04",
+    "url": "/face-detection/static/js/main.4be4d33e.chunk.js"
+  },
+  {
+    "revision": "dcade79a28a11225392c",
+    "url": "/face-detection/static/js/runtime-main.564577fb.js"
+  },
+  {
+    "revision": "57fa854e33d73bdf6d142841b75b5b43",
+    "url": "/face-detection/static/media/papyrus.57fa854e.png"
+  }
+]);
